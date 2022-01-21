cb57568d0c81d8659b5634d6fdd6922e4ac844c8
SayantanRC <sayantan_rc@protonmail.com>
2022-01-21 17:02:38 +0530

    add comments in main viewmodel

--- app/src/main/java/designstring/androidninja/ui/activities/MainViewModel.kt
+++ app/src/main/java/designstring/androidninja/ui/activities/MainViewModel.kt
@@ -19,11 +19,21 @@ constructor(
 
     private var pageNumber: Int = 1
 
+    /**
+     * Internal data set.
+     */
     private val peopleList = ArrayList<DomainEntity>(0)
 
+    /**
+     * Live data which the main activity observes for new data.
+     */
     private var _listOfPeopleLiveData = MutableLiveData<Triple<List<DomainEntity>, Int, Int>>()
     var listOfPeopleLiveData: LiveData<Triple<List<DomainEntity>, Int, Int>> = _listOfPeopleLiveData
 
+    /**
+     * Function to call [repository] method to load the next set of data.
+     * Also uses the [networkEntityMapper] to convert the received data to domain model.
+     */
     suspend fun getNextSetOfPeople() {
         val newList = repository.getPeopleDataFromNetwork(pageNumber++).let {
             networkEntityMapper.toDomainEntityList(it)
@@ -34,6 +44,9 @@ constructor(
         _listOfPeopleLiveData.value = Triple(peopleList, lastPosition, itemCount)
     }
 
+    /**
+     * If [peopleList] is empty, then fill the data of the first page from api request.
+     */
     suspend fun initPeopleSet(){
         if (peopleList.isEmpty()) {
             pageNumber = 1
