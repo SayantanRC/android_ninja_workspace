fbbb15f572b3e64e012ad1ddadd0bf3c02262b34
SayantanRC <sayantan_rc@protonmail.com>
2022-01-21 13:38:15 +0530

    make NetworkEntityMapper.kt injectable

--- app/src/main/java/designstring/androidninja/domain/mappers/NetworkEntityMapper.kt
+++ app/src/main/java/designstring/androidninja/domain/mappers/NetworkEntityMapper.kt
@@ -3,8 +3,12 @@ package designstring.androidninja.domain.mappers
 import designstring.androidninja.data.network.models.NetworkEntityPeople
 import designstring.androidninja.domain.interfaces.EntityMapper
 import designstring.androidninja.domain.models.DomainEntity
+import javax.inject.Inject
 
-class NetworkEntityMapper: EntityMapper<NetworkEntityPeople, DomainEntity> {
+class NetworkEntityMapper
+@Inject
+constructor()
+    : EntityMapper<NetworkEntityPeople, DomainEntity> {
     override fun toDomainEntity(otherEntity: NetworkEntityPeople): DomainEntity {
         return DomainEntity(
             otherEntity.first_name,
