816c03c09895838324214a268e3b3cfccba6861f
SayantanRC <sayantan_rc@protonmail.com>
2022-01-21 15:33:52 +0530

    Create viewmodel for main activity

---
+++ app/src/main/java/designstring/androidninja/ui/activities/MainViewModel.kt
@@ -0,0 +1,33 @@
+package designstring.androidninja.ui.activities
+
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+import dagger.hilt.android.lifecycle.HiltViewModel
+import designstring.androidninja.data.Repository
+import designstring.androidninja.domain.mappers.NetworkEntityMapper
+import designstring.androidninja.domain.models.DomainEntity
+
+@HiltViewModel
+class MainViewModel
+constructor(
+    private val networkEntityMapper: NetworkEntityMapper,
+    private val repository: Repository,
+): ViewModel() {
+
+    private var pageNumber: Int = 1
+
+    private val peopleList = ArrayList<DomainEntity>(0)
+
+    private var _listOfPeopleLiveData = MutableLiveData<List<DomainEntity>>()
+    var listOfPeopleLiveData: LiveData<List<DomainEntity>> = _listOfPeopleLiveData
+
+    suspend fun getNextSetOfPeople() {
+        val newList = repository.getPeopleDataFromNetwork(pageNumber++).let {
+            networkEntityMapper.toDomainEntityList(it)
+        }
+        peopleList.addAll(newList)
+        _listOfPeopleLiveData.value = peopleList
+    }
+
+}
