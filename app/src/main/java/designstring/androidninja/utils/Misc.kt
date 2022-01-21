7e570f81b8f0d6eb5adc51ba3087e2b5100ab2c4
SayantanRC <sayantan_rc@protonmail.com>
2022-01-21 15:59:44 +0530

    create a tryIt utility function

--- app/src/main/java/designstring/androidninja/utils/Misc.kt
+++ app/src/main/java/designstring/androidninja/utils/Misc.kt
@@ -1,9 +1,19 @@
 package designstring.androidninja.utils
 
+import android.content.Context
 import android.util.Log
+import android.widget.Toast
 
 private val TAG = "AndroidNinja"
 
 fun debugLog(message: String){
     Log.d(TAG, message)
+}
+
+fun tryIt(f: () -> Unit, context: Context? = null){
+    try {
+        f()
+    } catch (e: Exception){
+        context?.let { Toast.makeText(it, "Error: ${e.message}", Toast.LENGTH_SHORT).show() }
+    }
 }
