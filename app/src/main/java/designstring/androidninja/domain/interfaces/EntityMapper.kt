756a536125a1bfc644d776a0ffc5a4bf83607bc4
SayantanRC <sayantan_rc@protonmail.com>
2022-01-21 12:21:59 +0530

    create EntityMapper.kt interface

---
+++ app/src/main/java/designstring/androidninja/domain/interfaces/EntityMapper.kt
@@ -0,0 +1,13 @@
+package designstring.androidninja.domain.interfaces
+
+/**
+ * This interface is not useful much if only one type of conversion is needed,
+ * as in this case only from network entity to domain entity for showing to the user.
+ * But would have been useful if there was databases and POST requests.
+ */
+interface EntityMapper<Entity, DomainEntity> {
+
+    fun toDomainEntity(otherEntity: Entity): DomainEntity
+    fun fromDomainEntity(domainEntity: DomainEntity): Entity
+
+}
