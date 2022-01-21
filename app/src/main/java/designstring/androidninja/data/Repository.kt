1fa614857d1b8618fa250e50fa604c621442d1a7
SayantanRC <sayantan_rc@protonmail.com>
2022-01-21 13:36:23 +0530

    Create Repository class to make call to network

---
+++ app/src/main/java/designstring/androidninja/data/Repository.kt
@@ -0,0 +1,18 @@
+package designstring.androidninja.data
+
+import designstring.androidninja.data.models.NetworkEntityPeople
+import designstring.androidninja.data.retrofit.retrofitImpl
+import javax.inject.Inject
+
+class Repository
+@Inject
+constructor()
+{
+
+    suspend fun getPeopleDataFromNetwork(pageNo: Int = 1): List<NetworkEntityPeople>{
+        return retrofitImpl.queryPeople(
+            6, pageNo
+        ).peopleList
+    }
+
+}
