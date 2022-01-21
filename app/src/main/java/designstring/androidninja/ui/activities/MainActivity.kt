786f9dd40f6ee4e856b762fadfaf19864213ef73
SayantanRC <sayantan_rc@protonmail.com>
2022-01-21 16:00:52 +0530

    Populate MainActivity.kt

--- app/src/main/java/designstring/androidninja/ui/activities/MainActivity.kt
+++ app/src/main/java/designstring/androidninja/ui/activities/MainActivity.kt
@@ -2,11 +2,51 @@ package designstring.androidninja.ui.activities
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.activity.viewModels
+import androidx.lifecycle.lifecycleScope
+import dagger.hilt.android.AndroidEntryPoint
 import designstring.androidninja.R
+import designstring.androidninja.databinding.ActivityMainBinding
+import designstring.androidninja.domain.models.DomainEntity
+import designstring.androidninja.ui.adapters.RecyclerViewAdapter
+import designstring.androidninja.utils.tryIt
+import kotlinx.coroutines.launch
 
+@AndroidEntryPoint
 class MainActivity : AppCompatActivity() {
+
+    private val viewModel : MainViewModel by viewModels()
+
+    private lateinit var binding: ActivityMainBinding
+
+    private val peopleList = ArrayList<DomainEntity>(0)
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        val adapter = RecyclerViewAdapter(peopleList)
+        binding.peopleList.adapter = adapter
+
+        viewModel.listOfPeopleLiveData.observe(this){
+            peopleList.addAll(it)
+            adapter.notifyItemRangeChanged(peopleList.size - it.size, it.size)
+        }
+
+        tryIt({
+            lifecycleScope.launch {
+                viewModel.initPeopleSet()
+            }
+        }, this@MainActivity)
+
+        binding.loadMoreButton.setOnClickListener {
+            tryIt({
+                lifecycleScope.launch {
+                    viewModel.getNextSetOfPeople()
+                }
+            }, this)
+        }
     }
 }
